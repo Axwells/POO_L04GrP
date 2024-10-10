8421f13b63811e541b7b075247d01d3189aa79ea
Axwells <145656137+Axwells@users.noreply.github.com>
2024-10-10 15:46:20 +0200

    IntClass completed

--- src/main/java/ch/heigvd/dai/Main.java
+++ src/main/java/ch/heigvd/dai/Main.java
@@ -1,7 +1,20 @@
 package ch.heigvd.dai;
+import ch.heigvd.dai.intClass.IntClass;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+
+
+        //Test de la partie 2 : Classe int
+        IntClass myInt = new IntClass(12);
+        System.out.println("Valeur après instanciation: " + myInt.getValue());
+        myInt.setValue(-6408);
+        System.out.println("Valeur après modififcation: " + myInt.getValue());
+        System.out.println("Valeur en string: " + myInt.toString());
+
+
+
+
     }
 }
