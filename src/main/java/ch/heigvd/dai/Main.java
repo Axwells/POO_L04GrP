907d8670edadb1704e879b3487310c7a0ebb9526
Adam Kurt Gruber <adam.gruber@heig-vd.ch>
2024-10-16 20:52:58 +0200

    merge part 1

--- src/main/java/ch/heigvd/dai/Main.java
+++ src/main/java/ch/heigvd/dai/Main.java
@@ -1,20 +1,46 @@
 package ch.heigvd.dai;
-import ch.heigvd.dai.intClass.IntClass;
+
+import ch.heigvd.dai.tri_tableau_entiers.Sequence;
 
 public class Main {
     public static void main(String[] args) {
+        if (args.length == 0) return;
 
+        int[] sequence = new int[args.length];
 
+        // Pour chaque mot sequence doit prendre le chiffre et l'ajouter dans son vecteur
+        for (int i = 0; i < args.length; i++) {
+            sequence[i] = stringToInt(args[i]);
+        }
 
-        //Test de la partie 2 : Classe int
-        IntClass myInt = new IntClass(12);
-        System.out.println("Valeur après instanciation: " + myInt.getValue());
-        myInt.setValue(-6408);
-        System.out.println("Valeur après modififcation: " + myInt.getValue());
-        System.out.println("Valeur en string: " + myInt.toString());
+        Sequence bubble = new Sequence(sequence);
+        bubble.sort();
 
+        System.out.println(bubble);
+    }
 
+    public static int stringToInt(String str) {
+        int result = 0;
+        int sign = 1;
+        int i = 0;
 
+        // Vérification du signe + ou -
+        if (str.charAt(0) == '-') {
+            sign = -1;
+            i++;
+        } else if (str.charAt(0) == '+') {
+            i++;
+        }
 
+        for (; i < str.length(); i++) {
+            char c = str.charAt(i);
+
+            //ChatGPT
+            if (c < '0' || c > '9') {
+                throw new NumberFormatException("Invalid character: " + c);
+            }
+            result = result * 10 + (c - '0'); // allows multiple digits numbers
+        }
+        return result * sign;
     }
 }
